aab9b79f5a796f73e13a7b45b3a1223b942dfed7
santuparsi <santuparsi@gmail.com>
2025-06-15 20:33:06 +0530

    commit6_15

---
+++ Advance/C#12Features/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_12Features
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
